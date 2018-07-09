f265225cf75c707f80db93ff4c5af92e67ea1793
NesterJoseph <nesterjv@gmail.com>
2018-07-09 14:17:36 +0530

    adding 5 new functions

--- Utility 1.py
+++ Utility 1.py
@@ -104,7 +104,7 @@ def selector_Dict(input_dict):
             output_dict.update(output_dict_value)
             
     return output_dict
-
+# need to resolve
 
 def increase_decrease_test(real_number):
     if real_number > 0:
@@ -123,3 +123,113 @@ def increase_decrease_test(real_number):
     return output_list
 
 #print(increase_decrease_test(-20))
+
+def Phraze(list_name):
+    phraze_0 = ""
+    for i in list_name:
+        if len(list_name) == 1:
+            phraze_0 = str(i)
+        elif len(list_name) == 2:
+            if list_name.index(i) == 0:
+                phraze_0 = "{} ".format(i)
+            else:
+                phraze_0 += "and {}".format(i)
+        else:
+            if list_name.index(i) != len(list_name) - 1:
+                phrase = "{}, ".format(i)
+                phraze_0 += phrase
+            elif list_name.index(i) == len(list_name) - 2:
+                phrase_0 = "{} ".format(i)
+                phraze_0 += phrase_0
+            else:
+                phraze_0 += "and {}".format(i)
+    return phraze_0
+
+#print(Phraze([1,2,4,5,6]))
+
+def dict_subtractor(dict_1,dict_2,demultiplier):
+    """Works only when the keys of both the dictionaries are the same."""
+    """Format: dict_1 - dict_2"""
+    difference_dict = {}
+    for keys,values in dict_1.items():
+        difference = (values - dict_2[keys])/demultiplier
+        difference_element = {keys:difference}
+        difference_dict.update(difference_element)
+        
+    return difference_dict
+
+def dict_percentage_calculator(dict_1,dict_2):
+    """Works only when the keys of both the dictionaries are the same."""
+    """Format: (dict_1 - dict_2)/dict_2 * 100"""
+    percent_difference_dict = {}
+    for keys,values in dict_1.items():
+        if dict_2[keys] != 0:
+            percent_difference = (values - dict_2[keys])/dict_2[keys] * 100
+            percent_difference_element = {keys:percent_difference}
+        else:
+            percent_difference = 0
+            percent_difference_element = {keys:percent_difference}
+        percent_difference_dict.update(percent_difference_element)
+        
+    return percent_difference_dict
+
+def number_to_rupees_indian(value):
+    round_value = round(value,0)
+    
+    length_value = len(str(round_value)) - 2
+    
+    new_value = str(round_value)[:length_value]
+    
+    rupee_value = 0
+    if length_value <= 3:
+        rupee_value = 'Rs.' + str(round_value)
+    
+    if 3 < length_value <= 5:
+        rupee_value = 'Rs.' + '{:,.0f}'.format(int(new_value))
+        
+    if 5 < length_value <= 7:
+        rupee_value = 'Rs.' + str(round(int(new_value)/10**5, 2)) + ' lakhs'
+        
+    if 7 < length_value :
+        rupee_value = 'Rs.' + str(round(int(new_value)/10**7, 2)) + ' crores'
+    
+    return rupee_value
+
+def number_to_dollars(value):
+    
+    if value >= 0:
+        value = value
+        sign = 'positive'
+    else:
+        value = abs(value)
+        sign = 'negative'
+        
+    round_value = round(value,0)
+    
+    if '.' in str(round_value):
+        length_value = len(str(round_value)) - 2
+    else:
+        length_value = len(str(round_value))
+    
+    new_value = str(round_value)[:length_value]
+    
+    dollar_value = 0
+    if length_value <= 3:
+        dollar_value = '$' + str(new_value)
+    
+    if 3 < length_value <= 6:
+        dollar_value = '$' + '{:,.0f}'.format(int(new_value))
+        
+    if 6 < length_value <= 9:
+        dollar_value = '$' + str(round(int(new_value)/10**6, 2)) + ' million'
+        
+    if 9 < length_value :
+        dollar_value = '$' + str(round(int(new_value)/10**9, 2)) + ' billion'
+        
+    if sign == 'negative':
+        dollar_value_with_sign = '-' + dollar_value
+    else:
+        dollar_value_with_sign = dollar_value
+    
+    return dollar_value_with_sign
+
